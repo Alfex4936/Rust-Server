b51858645bac62da8ffdd7484a8a22e94900512e
seok <ikr@kakao.com>
2023-02-13 14:18:12 +0900

    Update lib.rs

--- auto_parser/src/lib.rs
+++ auto_parser/src/lib.rs
@@ -8,7 +8,7 @@ use serde::{Deserialize, Serialize};
 
 pub const AJOU_LINK: &str = "https://www.ajou.ac.kr/kr/ajou/notice.do";
 pub const MY_USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36";
-pub const MONGO_URL: &str = "mongodb+srv://***REMOVED***/myFirstDatabase?retryWrites=true&w=majority";
+pub const MONGO_URL: &str = "";
 
 #[derive(Serialize, Deserialize, Debug, Default, Clone)]
 pub struct Notice {
